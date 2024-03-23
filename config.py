447d6d169ae84dd2eff2543f8fb9c0807d3f2508
saharkhmissi11 <saharkhmissi90@gmail.com>
2024-03-23 23:02:49 +0100

    v1.1

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 DATABASE_CONFIG = {
-    "host": "localhost",
-    "port": 3306,
-    "user": "root",
-    "password": "127SSS*a127",
-    "database": "chatserver"
+    "host": "",
+    "port": ,
+    "user": "",
+    "password": "",
+    "database": ""
 }
